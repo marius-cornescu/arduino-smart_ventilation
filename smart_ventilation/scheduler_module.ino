49ce68cccb41313f51efecd63b650a5ad112564a
Marius Cornescu <marius_cornescu@yahoo.com>
2023-08-05 21:25:02 +0200

    Add grace period to scheduled switches, to prevent flicker

--- smart_ventilation/scheduler_module.ino
+++ smart_ventilation/scheduler_module.ino
@@ -4,10 +4,12 @@
 
 //= CONSTANTS ======================================================================================
 const byte VENT_STATUS_PIN = 4;  // DIGITAL PORT 4
+const unsigned long SCHEDULER_GRACE_PERIOD = 60 * SEC;  // how long to wait until registering change in scheduler
 //----------------------------------
 
 //= VARIABLES ======================================================================================
 static unsigned int lastVentStatusState = HIGH;
+unsigned long lastVentStatusUpdateMillis = 0;
 
 //==================================================================================================
 //**************************************************************************************************
@@ -17,6 +19,7 @@ void scheduler_Setup() {
 #endif
   //..............................
   pinMode(VENT_STATUS_PIN, INPUT);
+  lastVentStatusUpdateMillis = 5 * SCHEDULER_GRACE_PERIOD; // ignore updates in the begining / let the system stabilize
   //..............................
 #ifdef DEBUG
   Serial.println(F("SCHEDULER:Setup <<<"));
@@ -26,10 +29,15 @@ void scheduler_Setup() {
 //==================================================================================================
 void scheduler_CheckStatus() {
   unsigned int current = digitalRead(VENT_STATUS_PIN);
-  if (current != lastVentStatusState) {
+  if (__IsAlowedToChange() && current != lastVentStatusState) {
     lastVentStatusState = current;
+    lastVentStatusUpdateMillis = millis();
     actions_onSchedulerStatusChange(lastVentStatusState == HIGH);
   }
+}
+//==================================================================================================
+bool __IsAlowedToChange() {
+  return millis() - lastVentStatusUpdateMillis > SCHEDULER_GRACE_PERIOD;
 }
 //==================================================================================================
 //==================================================================================================
